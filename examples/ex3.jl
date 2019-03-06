f1181962030e072208f35631ad07c213b6ff3f38
antonio <antonior92@gmail.com>
2019-03-06 16:11:03 +0100

    Incude optimization in problem 3

--- examples/ex3.jl
+++ examples/ex3.jl
@@ -27,6 +27,7 @@ function derivative_v(y, u, v; β=β0, γ=γ0)
 end
 
 # Generate Data
+srand(4)
 N = 200
 rep = 10
 v = σ0*rand(N+1)
@@ -66,11 +67,12 @@ k0 = 1
 k0_list = collect(k0:Int(N//M):k0+N-1)
 x0_list =[[x_i] for x_i in v[k0_list+1]]
 list_procs = ones(Int, M)
+y_list = [[yi] for yi in y[2:end]]
 
 θ = [β0, γ0]
 multiple_shoot = ms.MultipleShooting(g, h,
                                      x0_list,
-                                     [[yi] for yi in y[2:end]],
+                                     y_list,
                                      k0_list, θ)
 ms.cost_function(multiple_shoot)
 
@@ -94,3 +96,45 @@ end
 γ_aux = repeat(γ_range, 1, length(β_range))
 contour(β_aux, γ_aux, cost', fill=true, color=:viridis)
 scatter!([β0], [γ0])
+
+# Check optimized resuts
+function compute_MSE(; β=β0, γ=γ0)
+    y_pred = zeros(size(y))
+    copy!(y_pred, y)
+    for i = 2:N+1
+        y_pred[i] = prediction(y[i-1], u[i-1], v[i-1], β=β, γ=γ)
+    end
+    mse = sum((y - y_pred).^2)
+end
+
+srand(4)
+n_experiments = 10
+θ_list = Array{Float64}[]
+cost_list = Float64[]
+res_list = []
+θ0_list = [[β0 + 0.001*randn(); γ0+ 0.001*randn()] for i = 1:n_experiments]
+for θ0 in θ0_list
+    σ = 0.002
+    x0_list_modified = [zeros(1) for i = 1:M]
+    for i = 1:M
+        x0_list_modified[i] += x0_list[i] + σ*randn()
+    end
+    opt = ms.OptimizationProblem(g, h, x0_list_modified, y_list,
+                                 k0_list, θ0)
+
+    res = ms.solve(opt, options=Dict("gtol" => 1e-10,
+                                     "xtol" => 1e-10,
+                                     "maxiter" => 2000,
+                                     "initial_tr_radius" => 0.01))
+    θ_est = res["x"][1:2]
+    cost_est = res["fun"]
+
+
+    println(θ_est)
+    push!(θ_list, θ_est)
+    push!(cost_list, cost_est)
+    push!(res_list, res)
+end
+
+
+MSE = [compute_MSE(β=θi[1], γ=θi[2])  for θi in θ_list]
