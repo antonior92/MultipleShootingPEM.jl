70b755bdb278feb8c67af80509c2af62a9343c37
antonio <antonior92@gmail.com>
2019-04-18 14:43:34 +0200

    reduce noise level

--- examples/ex3/ex3_generate_solutions.jl
+++ examples/ex3/ex3_generate_solutions.jl
@@ -50,7 +50,7 @@ d = Dict("data_generator" => Dict(:t=>"pendulum", :N=>N,
 push!(options_dicts, d)
 # Full turns around center
 d = Dict("data_generator" => Dict(:t=>"pendulum", :N=>N,
-                                  :σw=>0., :σv=>0.1,
+                                  :σw=>0., :σv=>0.09,
                                   :ampl=>50, :rep=>16,
                                   :seed=>1),
          "pem" => Dict(:model=>"output_error"),
