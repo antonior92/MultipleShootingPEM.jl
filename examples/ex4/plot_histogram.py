5ee3b3579f36b6d957732ac7e43a0691cddc478b
Antonio Horta Ribeiro <antonior92@gmail.com>
2020-01-06 17:35:18 -0300

    allow plot of multiple histograms

--- examples/ex4/plot_histogram.py
+++ examples/ex4/plot_histogram.py
@@ -4,13 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from base import THETA
 
+type = ['noe', 'multistage_2', 'multistage_5', 'narx']
 
-solutions = pd.read_csv('./narx_0.05_solutions.csv')
 fig, ax = plt.subplots(nrows=3)
-ax[0].hist(solutions['y[k-1]'])
+
+for t in type:
+    solutions = pd.read_csv('./'+ t + '_0.05_solutions.csv')
+    ax[0].hist(solutions['y[k-1]'], label=t)
+    ax[1].hist(solutions['y[k-2]'], label=t)
+    ax[2].hist(solutions['u[k-1]'], label=t)
+
 ax[0].axvline(THETA[0])
-ax[1].hist(solutions['y[k-2]'])
 ax[1].axvline(THETA[1])
-ax[2].hist(solutions['u[k-1]'])
 ax[2].axvline(THETA[2])
+plt.legend()
 plt.show()
