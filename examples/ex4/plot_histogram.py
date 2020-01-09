e7a9637709d30b00d5b096e3010d4cccc53fc62b
Antonio Horta Ribeiro <antonior92@gmail.com>
2020-01-09 14:13:29 -0300

    Small modifications on ploting

--- examples/ex4/plot_histogram.py
+++ examples/ex4/plot_histogram.py
@@ -2,20 +2,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from base import THETA
+from base import THETA1
 
-type = ['noe', 'multistage_2', 'multistage_5', 'narx']
+type = ['multistage_2', 'multistage_3', 'multistage_4', 'multistage_5']
 
 fig, ax = plt.subplots(nrows=3)
 
 for t in type:
-    solutions = pd.read_csv('./'+ t + '_0.05_solutions.csv')
+    solutions = pd.read_csv('./'+ t + '_0.1_solutions.csv')
     ax[0].hist(solutions['y[k-1]'], label=t)
-    ax[1].hist(solutions['y[k-2]'], label=t)
-    ax[2].hist(solutions['u[k-1]'], label=t)
+    ax[1].hist(solutions[' y[k-2]'], label=t)
+    ax[2].hist(solutions[' u[k-1]'], label=t)
 
-ax[0].axvline(THETA[0])
-ax[1].axvline(THETA[1])
-ax[2].axvline(THETA[2])
+ax[0].axvline(THETA1[0])
+ax[1].axvline(THETA1[1])
+ax[2].axvline(THETA1[2])
 plt.legend()
 plt.show()
