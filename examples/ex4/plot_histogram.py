2fd897152711116b9e05807f16970c3a8ded2216
Antonio Horta Ribeiro <antonior92@gmail.com>
2020-03-09 18:51:55 -0300

    Update plot histogram

--- examples/ex4/plot_histogram.py
+++ examples/ex4/plot_histogram.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from base import THETA1, THETA2, THETA3
 
 time_const = 'slow'
-list_n_stage = [2, 3, 4, 5, 6, 7, 9, 10]
+list_n_stage = [2, 3, 4, 5, 6, 7, 9, 10, 20]
 list_shoot_len = [2, 5, 10, 20]
-lateral = 4.0
-noise_std = 0.02
+lateral = 0.
+noise_std = 0.05
 folder = './solutions/'
 
 if time_const == 'interm':
@@ -18,22 +18,34 @@ if time_const == 'fast':
 elif time_const == 'slow':
     THETA = THETA3
 
-
+vars = ['y[k-1]', 'y[k-2]', 'u[k-1]']
 tp = []
 tp += [folder + 'multipleshoot_cp_100/multipleshoot_{}'.format(shoot_len) for shoot_len in list_shoot_len]
-#tp += [folder + 'multistage/multistage_{}'.format(n_stage) for n_stage in list_n_stage]
-#r = [[THETA[i] - lateral, THETA[i] + lateral] for i in range(3)]
-bins = 30
+tp += [folder + 'multistage/multistage_{}'.format(n_stage) for n_stage in list_n_stage]
+bins = 150
+rg = 0.015
+thres = 0.5 *rg
+logscale = 500
 fig, ax = plt.subplots(nrows=3)
+
 for t in tp:
     solutions = pd.read_csv('' + t + '_'+ str(noise_std) + '_' + time_const + '_solutions.csv')
 
-    ax[0].hist(solutions['y[k-1]'], label=t, bins=bins)#, range=r[0])
-    ax[1].hist(solutions['y[k-2]'], label=t, bins=bins)#, range=r[1])
-    ax[2].hist(solutions['u[k-1]'], label=t, bins=bins)#, range=r[2])
+    for i, v in enumerate(vars):
+        normalized_value = solutions[vars[i]] - THETA[i]
+        two_scales = np.zeros_like(normalized_value)
+        for j in range(len(normalized_value)):
+            if normalized_value[j] > thres:
+                two_scales[j] = np.log(normalized_value[j]-thres)/logscale + thres
+            elif thres > normalized_value[j] > -thres:
+                two_scales[j] = normalized_value[j]
+            else:
+                two_scales[j] = -np.log(thres-normalized_value[j])/logscale - thres
+        ax[i].hist(two_scales, label=t, bins=bins, range=[-rg, rg])
+        ax[i].set_xlim([-rg, rg])
+        ax[i].axvline(thres)
+        ax[i].axvline(0)
+        ax[i].axvline(-thres)
 
-for i in range(2):
-    ax[i].axvline(THETA[i])
-    #ax[i].set_xlim(r[i])
-#plt.legend()
+
 plt.show()
